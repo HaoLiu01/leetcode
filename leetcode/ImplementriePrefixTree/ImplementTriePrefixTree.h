cb47ab0513bc8aa2aa4183ccf9ab669ec001b985
Hao Liu <liuhaots@163.com>
2015-09-08 22:47:09 +0800

    [AddAndSearchWord + Trie]

---
+++ leetcode/ImplementriePrefixTree/ImplementTriePrefixTree.h
@@ -0,0 +1,23 @@
+/*
+ * ImplementTriePrefixTree.h
+ *
+ *  Created on: Sep 8, 2015
+ *      Author: hao
+ */
+
+#ifndef LEETCODE_IMPLEMENTRIEPREFIXTREE_IMPLEMENTTRIEPREFIXTREE_H_
+#define LEETCODE_IMPLEMENTRIEPREFIXTREE_IMPLEMENTTRIEPREFIXTREE_H_
+
+/**********************************************************************************
+ *
+ * Implement a trie with insert, search, and startsWith methods.
+ *
+ * Note:
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ *
+ *
+ **********************************************************************************/
+
+
+
+#endif /* LEETCODE_IMPLEMENTRIEPREFIXTREE_IMPLEMENTTRIEPREFIXTREE_H_ */
