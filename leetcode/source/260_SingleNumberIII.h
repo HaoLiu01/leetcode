6e8e050732ed52bfeaa046ca506a8a38eae10e15
Hao Liu <hao1iu@163.com>
2016-06-20 21:37:24 +0800

    Single Number III

--- leetcode/source/260_SingleNumberIII.h
+++ leetcode/source/260_SingleNumberIII.h
@@ -27,7 +27,31 @@ vector<int> singleNumber(vector<int>& nums) {
 //        eor ^= nums[i];
 //    }
 
+/*
+    首先计算nums数组中所有数字的异或，记为xor
+    令lowbit = xor & -xor，lowbit的含义为xor从低位向高位，第一个非0位所对应的数字
+    例如假设xor = 6（二进制：0110），则-xor为（二进制：1010，-6的补码，two's complement）
+    则lowbit = 2（二进制：0010）
+    根据异或运算的性质，“同0异1”
+    记只出现一次的两个数字分别为a与b
+    可知a & lowbit与b & lowbit的结果一定不同
+    通过这种方式，即可将a与b拆分开来*/
 
+    int eor = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        eor ^= nums[i];
+    }
+
+    int mask = eor & (~(eor - 1));
+    int A = 0, B = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (lowbit & nums[i])
+            A ^= nums[i];
+        else
+            B ^= nums[i];
+    }
+
+    return vector<int>( { A, B });
 }
 
 #endif /* LEETCODE_SOURCE_260_SINGLENUMBERIII_H_ */
