de6153fdc12957dbae97feff73615ed3afe2f4de
Hao Liu <hao1iu@163.com>
2016-07-23 22:24:26 +0800

    [238 productOfArrayExceptSelf] The solution without division

--- leetcode/source/238_productOfArrayExceptSelf.h
+++ leetcode/source/238_productOfArrayExceptSelf.h
@@ -23,38 +23,86 @@ not count as extra space for the purpose of space complexity analysis.)*/
 #include "common.h"
 
 // With division
+//vector<int> productExceptSelf(vector<int>& nums) {
+//    int totalProduct = nums[0];
+//    int countZero = 0;
+//    for (int i = 1; i < nums.size(); i++) {
+//        totalProduct *= nums[i];
+//        if (nums[i] == 0) {
+//            countZero ++;
+//            if (countZero > 1)
+//                break;
+//        }
+//    }
+//
+//    vector<int> res;
+//
+//    if (countZero > 1) {
+//        for (int i = 0; i < nums.size(); i++)
+//            res.push_back(0);
+//    } else {
+//        for (int i = 0; i < nums.size(); i++)
+//            if (nums[i] != 0) {
+//                res.push_back(totalProduct / nums[i]);
+//            } else {
+//                int newTotal = 1;
+//                for (int j = 0; j < nums.size(); j++) {
+//                    if (i != j)
+//                        newTotal *= nums[j];
+//                }
+//                res.push_back(newTotal);
+//            }
+//    }
+//
+//    return res;
+//}
+
 vector<int> productExceptSelf(vector<int>& nums) {
-    int totalProduct = nums[0];
-    int countZero = 0;
-    for (int i = 1; i < nums.size(); i++) {
-        totalProduct *= nums[i];
-        if (nums[i] == 0) {
-            countZero ++;
-            if (countZero > 1)
-                break;
-        }
-    }
+//    product of left elements * product of right elements
+    int *left = new int[nums.size()];
+    int *right = new int[nums.size()];
+    int maxIdx = nums.size() - 1;
+
+    left[0] = nums[0];
+    for (int i = 1; i < maxIdx; i++)
+        left[i] = nums[i] * left[i - 1];
+
+    right[maxIdx] = nums[maxIdx];
+    for (int i = maxIdx - 1; i > 0; i--)
+        right[i] = nums[i] * right[i + 1];
 
     vector<int> res;
+    res.push_back(right[1]);
+    for (int i = 1; i < maxIdx; i++)
+        res.push_back(left[i - 1] * right[i + 1]);
 
-    if (countZero > 1) {
-        for (int i = 0; i < nums.size(); i++)
-            res.push_back(0);
-    } else {
-        for (int i = 0; i < nums.size(); i++)
-            if (nums[i] != 0) {
-                res.push_back(totalProduct / nums[i]);
-            } else {
-                int newTotal = 1;
-                for (int j = 0; j < nums.size(); j++) {
-                    if (i != j)
-                        newTotal *= nums[j];
-                }
-                res.push_back(newTotal);
-            }
+    res.push_back(left[maxIdx - 1]);
+
+    delete left;
+    delete right;
+    return res;
+}
+
+// A better solution
+vector<int> productExceptSelf2(vector<int>& nums) {
+    int len = nums.size();
+    vector<int> left(len,  1);  //当前元素左边的乘积
+    vector<int> right(len, 1);  //当前元素右边的乘积
+    vector<int> result;
+
+    for(int i=1; i<len; i++){
+        left[i] = left[i-1]*nums[i-1];
     }
 
-    return res;
+    for(int i=len-2; i>=0; i--){
+        right[i] = right[i+1]*nums[i+1];
+    }
+
+    for(int i=0; i<len; i++){
+        result.push_back(left[i] * right[i]);
+    }
+
+    return result;
 }
 
 #endif /* LEETCODE_SOURCE_238_PRODUCTOFARRAYEXCEPTSELF_H_ */
